89b6c2181a919b8d1797594de2a9ec11b84c2184
Efrelle Joyce Ducyao <efrellejoyced@gmail.com>
2024-11-04 16:23:48 +0700

    userProfile.js

--- README.md
+++
@@ -1,1 +0,0 @@
-# Coding-Problem-1
---
+++ userProfile.js
@@ -0,0 +1,9 @@
+let fullName = "Efrelle Joyce C. Ducyao";
+let age = 20;
+let favoriteNumber = 15;
+let favoriteColor = "blue";
+
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
